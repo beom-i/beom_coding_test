74afc79da6d20acf197053d84fe2ac338b67f3eb
beom-i <55353401+beom-i@users.noreply.github.com>
2023-05-03 12:30:54 +0900

    Update 에디터.cc
    update description of code

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -7,7 +7,13 @@
 #include <list>
 
 
+//이 문제는 뭔가 연결리스트의 근본을 알 수있었다. 나는 애초에 stl list에서 추가나 삭제의함수가 실행되었을때
+//iterator의 행방이 궁금했다. 배운 점은 이렇고
+//1. iterator가 end를 가르키고 있을 때 push_back()하면 iterator은 자동으로 밀림 end로
+//2. insert는 iterator의 왼쪽으로 삽입
+//3. erase를 하면 반복자를 무조건 재할당 해야됨(iterator = mylist.erase(iterator);)처럼
 
+//이 문제에 관해서는 끝의 경우를 생각하지 않아도 됨 내가 한 이유는 stl list를 충분히 알지 못했다.
 using namespace std;
 
 int main() {
@@ -43,7 +49,7 @@ int main() {
                 mylist.pop_back();
             }
             else if(iter!=mylist.begin()) { 
-                iter --; //이게 문제네
+                iter --;
                 iter = mylist.erase(iter);
             }
         }

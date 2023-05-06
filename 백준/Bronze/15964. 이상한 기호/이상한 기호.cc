9a7e6dd70503b6e1f69eae7bc537541e7ff10232
beom-i <55353401+beom-i@users.noreply.github.com>
2023-05-06 13:12:59 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <cmath>
+#include <list>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<(a+b)*(a-b);
+    return 0;
+}

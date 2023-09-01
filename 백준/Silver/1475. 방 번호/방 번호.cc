7d21efeba892537ac8215f3882a92e98fb3f75bc
beom-i <55353401+beom-i@users.noreply.github.com>
2023-09-01 17:26:07 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,48 +1,41 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <stdio.h>
 #include <string.h>
-
-
+#include <algorithm>
+#include <cmath>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <sstream>
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int N;
-    cin >> N;
-    int digits = 0;
-    int temp = N;
-    while (temp != 0) {
-        digits++;
-        temp /= 10;
+    cout.tie(0);
+    int arr[10]={0};
+    string str;
+    cin>>str;
+    int max=0;
+    for(int i = 0 ; i<str.length();i++){
+        int n = str[i] -'0';
+        arr[n] = arr[n] + 1;
     }
 
-    // 정수형 배열 할당
-    int arr[digits];
-    int sor[10] = {};
-
-    for(int i = 0 ; i < digits ; i++){
-        arr[i] = N%10;
-        sor[arr[i]] = sor[arr[i]] + 1;
-        N = N / 10; 
-    }
-
-    int sum = sor[6] + sor[9];
-    if(sum % 2 == 1){
-        sor[6] = sum/2+1;
-        sor[9] = sum/2+1;
-    }
-    else if(sum % 2 == 0){
-        sor[6] = sum/2;
-        sor[9] = sum/2;
-    }
-
-    int storage =0;
-    for (int i = 0; i< 10; i++) {
-        if( storage < sor[i]){
-            storage = sor[i];
+    //6과 9의 예외처리
+    int sum=0;
+    if((arr[6]+arr[9])%2==0) sum = (arr[6]+arr[9])/2;
+    else sum = (arr[6]+arr[9])/2+1;
+    arr[6] = sum;
+    arr[9] = sum;
+    for(int i = 0 ; i < 10;i++){
+        if(max < arr[i]){
+            max = arr[i];
         }
     }
-    cout << storage;
+    cout<<max;
+    return 0;
 }

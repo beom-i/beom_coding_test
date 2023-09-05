9203e01c211a76023fd14fdfb44d7f0064e46429
beom-i <55353401+beom-i@users.noreply.github.com>
2023-09-05 10:09:37 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 60 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <stdio.h>
 #include <string.h>
 #include <algorithm>
 #include <cmath>
 #include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <sstream>
+#include <iomanip> // std::fixed 및 std::setprecision을 사용하기 위해 필요
 
 using namespace std;
 
@@ -12,37 +18,27 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    list<int> array;  
-    int N,K,sum=0;
+    queue<int> arr;
+    vector<int> vec;
+    int N,K;
     cin >> N >> K;
-    for(int i = 1 ; i<= N;i++){
-        array.push_back(i);
+    for(int i = 1 ; i <= N;i++){
+        arr.push(i);
     }
-    cout << "<";
-    list<int>::iterator iter = array.begin(); 
-    int count = 1;
-    while(!array.empty()){
-        if(iter == array.end()){
-            iter = array.begin();
+    while(arr.size()!=0){
+        for(int i = 1;i<K;i++){
+            arr.push(arr.front());
+            arr.pop();
         }
-        if(count == K){
-            cout<<*iter;
-            iter = array.erase(iter);
-            count=1;
-            if(K == 1){
-                count --;
-            }
-            if(array.empty()) break;
-
-            cout << ", ";
-
-        }
-        if(iter != array.end()) {
-            count++;
-            iter++;
-            if(K==1) iter --;
-        }
+        vec.push_back(arr.front());
+        arr.pop();
     }
-    cout << ">";
+    cout <<"<";
+    for(int i =0;i<vec.size();i++){
+        cout<<vec[i];
+        if(i==vec.size()-1)break;
+        cout<<", ";
+    }
+    cout<<">";
     return 0;
 }

dc51eae40f6d916a6d5de1db24ca9d88380248c6
beom-i <55353401+beom-i@users.noreply.github.com>
2024-01-22 20:35:38 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <cmath>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <set>
+#include <map>
+#include <deque>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int N;
+    cin>>N;
+    if(N%2==0) cout<<"CY";
+    else cout << "SK";
+}

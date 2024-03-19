1c35ef918c54783c012e6771f51c21a92c938414
beom-i <55353401+beom-i@users.noreply.github.com>
2024-03-19 15:05:15 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 2388 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -21,48 +21,52 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int arr[501][501];
-    int n,m;
-    cin >> n>>m;
+    bool board[501][501]={0};
     bool visit[501][501]={0};
-    int dx[4] = {1, 0, -1, 0}; // 오른쪽, 아래, 왼쪽, 위쪽
-    int dy[4] = {0, 1, 0, -1};
-    for(int i = 0 ; i < n ; i++){
-        for(int j=0; j < m ; j ++){
-            int number;
-            cin >> number;
-            arr[i][j] = number;
+    int dx[4] = {1,0,-1,0};
+    int dy[4] = {0,1,0,-1};
+    int N, M;
+    cin >> N >> M;
+    for(int i = 1;i <= N;i++){
+        for(int j = 1 ;j <=M;j++){
+            int num;
+            cin >> num;
+            board[i][j]=num;
         }
     }
-    int count = 0;
-    int max = 0;
+
     queue<pair<int,int>> Q;
-    for(int y = 0 ; y < n ; y ++){
-        for(int x = 0 ; x < m ; x ++){
-            // 방문했거나 그림이 없으면 바로 넘겨
-            if(visit[y][x] == 1 || arr[y][x] != 1) continue;
-            visit[y][x] = 1;
-            Q.push({y,x});
-            int cur_max=0;
-            while(!Q.empty()){
-                cur_max++;
-                pair<int,int> cur = Q.front();
-                Q.pop();
-                for(int dir = 0 ; dir < 4; dir++){
-                    int ny = cur.first + dy[dir];
-                    int nx = cur.second + dx[dir];
-                    if(ny<0 || ny>=n || nx <0 || nx>=m) continue;
-                    if(visit[ny][nx] == 0 && arr[ny][nx] == 1) {
-                        visit[ny][nx] = 1;
-                        Q.push({ny,nx});
+    int entire_count = 0; //  전체 그림 개수
+    int max_area = 0;
+    for(int i = 1 ; i <=N;i++){
+        for(int j = 1 ; j <=M ; j++){
+            // 보드에 1이고, 방문한 적 없으면
+            if(visit[i][j]==0 && board[i][j]==1){
+                entire_count++;
+                int cur_area=0;
+                Q.push({i,j});
+                visit[i][j]=1;
+                while(!Q.empty()){
+                    pair<int,int> cur = Q.front();Q.pop();
+                    for(int c = 0 ; c <4 ; c ++){
+                        int nx = cur.first + dx[c];
+                        int ny = cur.second + dy[c];
+                        // 범위 밖
+                        if(nx<0 || nx > N || ny <0 || ny>M) continue;
+                        // 방문했거나, 보드에 없으면 넣지 않음
+                        if(visit[nx][ny]==1 || board[nx][ny]!=1) continue;
+                        visit[nx][ny]=1;
+                        Q.push({nx,ny});
                     }
+                    cur_area++;
                 }
+                if(max_area<cur_area){max_area = cur_area;}
+                
             }
-            if(max<cur_max) max = cur_max;
-            if(cur_max>0) count++;
         }
     }
-    cout<<count<<'\n'<<max;
+
+    cout<<entire_count<<'\n'<<max_area;
 
     return 0;
 }

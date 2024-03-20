67f2a8370299536a3987a0f9dea0b56a52402e6f
beom-i <55353401+beom-i@users.noreply.github.com>
2024-03-20 16:15:53 +0900

    [Gold V] Title: 토마토, Time: 76 ms, Memory: 10092 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -21,44 +21,47 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int M,N;
-    cin >> M >> N;
-    int board[1002][1002];
-    int dy[4] = {0,0,1,-1};
-    int dx[4] = {1,-1,0,0}; // 오 왼 아래 위
+
     queue<pair<int,int>> Q;
-    // M : 가로
-    for(int i = 0 ; i < N ; i ++){
-        for( int j = 0 ; j < M ; j ++ ){
-            int number;
-            cin >> number;
-            // 토마토가 익은 경우 Q에 넣음
-            if(number == 1) Q.push({i,j});
-            board[i][j] = number;
+    int board[1001][1001] ={-1};
+    int width,length;
+    cin >> width >> length;
+    int dx[4] = {1,0,-1,0};
+    int dy[4] = {0,1,0,-1};
+    for(int i = 1 ; i <= length;i++){ 
+        for(int j = 1 ; j <= width; j++){
+            int num;
+            cin>>num;
+            board[i][j] = num;
+            if(num == 1) Q.push({i,j});
         }
     }
     while(!Q.empty()){
-        pair<int,int> cur = Q.front();
-		Q.pop();
-        for(int dir = 0 ; dir < 4 ; dir ++){
-            int ny = cur.first + dy[dir];
-            int nx = cur.second + dx[dir];
-            if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-            if(board[ny][nx] != 0) continue;
-            board[ny][nx] = board[cur.first][cur.second]+1;
-            Q.push({ny,nx});
+        auto cur = Q.front(); Q.pop();
+        for(int i = 0 ; i < 4 ; i ++){
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
+            if(nx < 1 || ny < 1 || nx > length || ny > width) continue;
+            if(board[nx][ny]!=0) continue;
+            board[nx][ny] = board[cur.first][cur.second]+1;
+            Q.push({nx,ny});
+            // for(int i = 1;i<=length;i++){
+            //     for(int j = 1 ; j <= width; j++){
+            //         cout<<board[i][j];
+            //     }
+            //     cout<<'\n';
+            // }
         }
     }
-    int max = 0; 
-    for(int i = 0 ; i < N;i++){
-        for(int j = 0 ; j < M ; j ++){
-            if(board[i][j] == 0){
-                cout << -1;
-                return 0;
-            }
-            if(max < board[i][j] ) max = board[i][j];
+    int max = 0;
+    for(int i = 1 ; i <= length;i++){ 
+        for(int j = 1 ; j <= width; j++){
+            if(board[i][j]==0) {cout<<-1;return 0;}
+            if(max<board[i][j]) max = board[i][j];
         }
     }
-    cout << max-1;
+    cout<<max-1;
     return 0;
 }
+
+

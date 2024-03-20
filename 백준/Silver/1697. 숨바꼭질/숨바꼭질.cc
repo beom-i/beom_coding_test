15e3139c995cf9bc9d54ca992c8f67716626fdcc
beom-i <55353401+beom-i@users.noreply.github.com>
2024-03-20 21:20:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2420 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -21,39 +21,29 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int arr[100001];
-    fill(arr,arr+100001,-1);
-    int subin, young;
-    cin >> subin >> young;
+
+    int subin[100002];
+    fill(subin,subin+100001,-1);
     queue<int> Q;
-    Q.push(subin);
-    arr[subin]=0;
+    int N,K;
+    cin >> N >> K;
+    Q.push(N);
+    subin[N] = 0;
     while(!Q.empty()){
-        auto cur = Q.front();
-        Q.pop();
-        int cur_1 = cur - 1;
-        int cur_2 = cur + 1;
-        int cur_3 = cur * 2;
-        if(cur < 0 || cur>100000) continue;
-        if( cur_1 >= 0){
-            if( arr[cur_1] > arr[cur] + 1 || arr[cur_1] < 0) {
-                arr[cur_1] = arr[cur]+1;
-                Q.push(cur_1);
-            }
-        }
-        if(cur_2 <= 100000){
-            if(arr[cur_2] > arr[cur] + 1 || arr[cur_2] < 0){
-                arr[cur_2] = arr[cur]+1;
-                Q.push(cur_2);
-            }
-        }
-        if(cur_3 <= 100000){
-            if(arr[cur_3] > arr[cur] + 1 || arr[cur_3] < 0){
-                arr[cur_3] = arr[cur]+1;
-                Q.push(cur_3);
-            }
+        int cur = Q.front();Q.pop();
+        if(cur == K) {cout<<subin[cur];return 0;}
+        for(int i = 0 ; i < 3 ; i ++){
+            int nx;
+            if(i==0)  nx = cur -1;
+            else if(i==1)  nx = cur + 1;
+            else if(i==2)  nx = cur * 2;
+            if(nx < 0 || nx > 100000) continue;
+            if(subin[nx] != -1) continue;
+            subin[nx]=subin[cur]+1;
+            Q.push(nx);
         }
     }
-    cout << arr[young];
     return 0;
 }
+
+
